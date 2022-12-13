a34a54a3124566ae23c25d3753badde4416192c6
opengl-8080 <opengl-8080@users.noreply.github.com>
2022-12-13 21:01:41 +0900

    Scriptable DataSetsを検証

---
+++ src/test/java/sandbox/dbrider/ScriptableDataSetsTest.java
@@ -0,0 +1,29 @@
+package sandbox.dbrider;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.junit5.api.DBRider;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+@DBRider
+public class ScriptableDataSetsTest {
+    @RegisterExtension
+    static DatabaseSupport db = new DatabaseSupport();
+
+    @BeforeAll
+    static void beforeAll() {
+        db.executeUpdate("create table foo_table (id integer, value varchar(32))");
+    }
+
+//    @Test
+    @DataSet("sandbox/dbrider/ScriptableDataSetsTest/testJavaScript/data-set.yml")
+    @ExpectedDataSet("sandbox/dbrider/ScriptableDataSetsTest/testJavaScript/expected.yml")
+    void testJavaScript() {}
+
+    @Test
+    @DataSet("sandbox/dbrider/ScriptableDataSetsTest/testGroovy/data-set.yml")
+    @ExpectedDataSet("sandbox/dbrider/ScriptableDataSetsTest/testGroovy/expected.yml")
+    void testGroovy() {}
+}
