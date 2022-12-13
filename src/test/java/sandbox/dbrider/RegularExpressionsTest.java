a103d5a29814188c6aeb2dcce07a836a13df4789
opengl-8080 <opengl-8080@users.noreply.github.com>
2022-12-13 20:12:11 +0900

    正規表現を検証

---
+++ src/test/java/sandbox/dbrider/RegularExpressionsTest.java
@@ -0,0 +1,24 @@
+package sandbox.dbrider;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.junit5.api.DBRider;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+@DBRider
+public class RegularExpressionsTest {
+    @RegisterExtension
+    static DatabaseSupport db = new DatabaseSupport();
+
+    @BeforeAll
+    static void beforeAll() {
+        db.executeUpdate("create table foo_table (id integer, value varchar(32))");
+    }
+
+    @Test
+    @DataSet("sandbox/dbrider/RegularExpressionsTest/data-set.yml")
+    @ExpectedDataSet("sandbox/dbrider/RegularExpressionsTest/expected.yml")
+    void test() {}
+}
